2489d22ae425806fdeb80eae03326c78e42c2151
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-08-16 11:49:02 +0800

    c

---
+++ backend/controller.js
@@ -0,0 +1,10 @@
+export const getUser = async (req, res) => {
+  async (req, res) => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users");
+      res.json(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import express from "express";
+import { getUser } from "./controller";
 
 const app = express();
 
@@ -7,14 +8,7 @@ app.get("/", (req, res) => {
   res.json("Hello Guys");
 });
 
-app.get("/user", async (req, res) => {
-  try {
-    const response = await axios.get("https://reqres.in/api/users");
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+app.get("/user", getUser);
 
 app.listen(5000, () => {
   console.log("Server On");
