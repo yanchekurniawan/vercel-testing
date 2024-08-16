3427817721dbd3ddbb4cd1071051e8cb93023e53
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-08-16 12:38:41 +0800

    g

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
 import express from "express";
-import { getUser } from "./controller";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.json("Hello Guys");
 });
-
-app.get("/user", getUser);
 
 app.listen(5000, () => {
   console.log("Server On");
